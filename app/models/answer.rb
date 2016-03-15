dbb772d8c6799b474a815f0f662311c7fa34b9f2
Baptiste Becmeur <baptistebecmeur@gmail.com>
2016-03-15 17:10:08 +0100

    cloudinary properly configured

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,2 +1,6 @@
 class Answer < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :picture, presence:true
+  has_many :questions,dependent: :destroy
+  mount_uploader :picture, PictureUploader
 end
